5d5a1aac79b1b3ed07a69edc46590fd341154e15
yu <934193155@qq.com>
2021-02-19 10:07:29 +0800

    更改title

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -5,7 +5,7 @@
             <i v-if="!collapse" class="el-icon-s-fold"></i>
             <i v-else class="el-icon-s-unfold"></i>
         </div>
-        <div class="logo">就业信息管理系统</div>
+        <div class="logo">就业信息系统</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 全屏显示 -->
